eb77c687b8dbc89f3006688ac94f1cca56d4b03f
fenago <socrates73@gmail.com>
2019-08-03 17:41:15 +0000

    Update runPrep.sh

--- jupyter-notebook/assets/runPrep.sh
+++ jupyter-notebook/assets/runPrep.sh
@@ -1,15 +1,15 @@
 # copy the script into the container; note: the resulting file is owned by root and it not executable
 docker cp prepareContainer.sh jupyter:/tmp/prepareContainerRoot.sh
-#docker cp iris.csv jupyter:/tmp/iris.csv
-#docker cp pima-indians-diabetes.csv jupyter:/tmp/pima-indians-diabetes.csv
-docker cp *.csv jupyter:/tmp/
-docker cp *.xlsx jupyter:/tmp/
+docker cp iris.csv jupyter:/tmp/iris.csv
+docker cp pima-indians-diabetes.csv jupyter:/tmp/pima-indians-diabetes.csv
+docker cp abalone.csv jupyter:/tmp/abalone.csv
+#docker cp *.xlsx jupyter:/tmp/
 # copy the script to make it accessible and executable 
 docker exec -it jupyter bash -c 'cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
-#docker exec -it jupyter bash -c 'cp /tmp/iris.csv ~/iris.csv'
-#docker exec -it jupyter bash -c 'cp /tmp/pima-indians-diabetes.csv ~/pima-indians-diabetes.csv'
-docker exec -it jupyter bash -c 'cp /tmp/*.csv ~/'
-docker exec -it jupyter bash -c 'cp /tmp/*.xlsx ~/'
+docker exec -it jupyter bash -c 'cp /tmp/iris.csv ~/iris.csv'
+docker exec -it jupyter bash -c 'cp /tmp/pima-indians-diabetes.csv ~/pima-indians-diabetes.csv'
+docker exec -it jupyter bash -c 'cp /tmp/abalone.csv ~/abalone.csv'
+#docker exec -it jupyter bash -c 'cp /tmp/*.xlsx ~/'
 # execute the script inside the container - to install a number of packages
 docker exec -it jupyter bash -c './prepareContainer.sh'
 # # restart the docker container
