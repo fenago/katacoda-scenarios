7083e06f2b9347def55b31bba20584c1fd0a4637
fenago <socrates73@gmail.com>
2019-08-03 03:05:36 +0000

    Update runPrep.sh

--- jupyter-notebook/assets/runPrep.sh
+++ jupyter-notebook/assets/runPrep.sh
@@ -1,9 +1,11 @@
 # copy the script into the container; note: the resulting file is owned by root and it not executable
 docker cp prepareContainer.sh jupyter:/tmp/prepareContainerRoot.sh
-docker cp prepareContainer.sh jupyter:~/iris.csv
-docker cp prepareContainer.sh jupyter:~/pima-indian-diabetes.csv
+docker cp iris.csv jupyter:/tmp/iris.csv
+docker cp pima-indians-diabetes.csv jupyter:/tmp/pima-indians-diabetes.csv
 # copy the script to make it accessible and executable 
 docker exec -it jupyter bash -c 'cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
+docker exec -it jupyter bash -c 'cp /tmp/iris.csv ~/iris.csv'
+docker exec -it jupyter bash -c 'cp /tmp/pima-indians-diabetes.csv ~/pima-indians-diabetes.csv'
 # execute the script inside the container - to install a number of packages
 docker exec -it jupyter bash -c './prepareContainer.sh'
 # # restart the docker container
