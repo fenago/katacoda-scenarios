7ab64ec9bcd72fd64a1ed3dcfbef55978a6458b1
Ather Tahir <ather.tahir@bentley.com>
2019-09-14 14:19:28 +0500

    Updated

--- azure-functions-java/set-env.sh
+++ azure-functions-java/set-env.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+yes | apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
 
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
     gpg --dearmor | \
@@ -10,4 +10,4 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     tee /etc/apt/sources.list.d/azure-cli.list
 apt-get update
-apt-get install azure-cli
+yes | apt-get install azure-cli
