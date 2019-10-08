15e4ac769253d81d5d9d6d2022a9669b66b5154d
akramsahmad <53534972+akramsahmad@users.noreply.github.com>
2019-10-08 16:19:13 +0500

    Update set-env.sh

--- XGBoost-with-python/chapter-4/set-env.sh
+++ XGBoost-with-python/chapter-4/set-env.sh
@@ -13,7 +13,7 @@ docker cp  ~/tutorial/. jupyter:/home/jovyan/work
 # copy the script to make it accessible and executable 
 docker exec -it jupyter bash -c 'cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
 # execute the script inside the container - to install a number of packages
-docker exec -it jupyter bash -c './prepareContainer.sh'
+docker exec -it jupyter bash -c './prepareContainer.s'
 # # restart the docker container
 docker restart jupyter
 echo 'restarted Jupyter container'
