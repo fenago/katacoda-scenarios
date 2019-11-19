973a585485146c6b83b00a5d8c337e7bad10caea
athertahir <31277617+athertahir@users.noreply.github.com>
2019-11-20 01:22:11 +0500

    Updated

--- machine-learning-mastery-weka/machine-learning-mastery-weka-chapter-22/set-env.sh
+++ machine-learning-mastery-weka/machine-learning-mastery-weka-chapter-22/set-env.sh
@@ -1,4 +1,4 @@
 
 docker run -d --network host --privileged --name kali-desktop athertahir/kali-weka
 
-docker exec -it kali-desktop bash -c "cp -r /usr/share/doc/weka/ /root/Desktop && cd /root/Desktop && wget https://netix.dl.sourceforge.net/project/weka/datasets/datasets-numeric/datasets-numeric.jar && unzip datasets-numeric.jar && cp /root/Desktop/numeric/housing.arff /root/Desktop/numeric/housing-numeric.arff && sed -i 's/@attribute CHAS { 0, 1}/@attribute CHAS real/g' /root/Desktop/numeric/housing-numeric.arff"
+docker exec -it kali-desktop bash -c "mkdir -p /root/Desktop/weka && cp -r /usr/share/doc/weka/ /root/Desktop && cd /root/Desktop && wget https://netix.dl.sourceforge.net/project/weka/datasets/datasets-numeric/datasets-numeric.jar && unzip datasets-numeric.jar && cp /root/Desktop/numeric/housing.arff /root/Desktop/numeric/housing-numeric.arff && sed -i 's/@attribute CHAS { 0, 1}/@attribute CHAS real/g' /root/Desktop/numeric/housing-numeric.arff"
